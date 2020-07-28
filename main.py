33192fe56fb17605aca2d7e1e7dfe23986124d12
Monnyka <31350922+Monnyka@users.noreply.github.com>
2020-07-28 22:54:45 +0700

    init fourth

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import requests
 import shutil
 import telebot
 
-total, used, free = shutil.disk_usage("d:/")
-report_disk_total="Total: %d GB" % (total // (2**30))
-report_disk_used="Used: %d GB" % (used // (2**30))
-report_disk_free="Free: %d GB" % (free // (2**30))
+# total, used, free = shutil.disk_usage("d:/")
+# report_disk_total="Total: %d GB" % (total // (2**30))
+# report_disk_used="Used: %d GB" % (used // (2**30))
+# report_disk_free="Free: %d GB" % (free // (2**30))
 bot = telebot.TeleBot('1107438786:AAED_PIcm4FYHNX_zCLJBGDktDVpAvf7g5Q')
 
 def telegram_bot_sendtext(bot_message):
@@ -19,9 +19,9 @@ def telegram_bot_sendtext(bot_message):
 
 def report():
     report_movie="*Report for Hydra Server:* \n\n ~~_MEDIA:_~~\n -New Added Movie:\n     *American Sniper (2020)*\n"
-    report_tv="-New Added Movie:\n     *Arrow S08E01*\n\n"
-    report_spacing="~~_DISK USAGE_~~\n -Total: \n      *{}*\n -Use: \n      *{}* \n-Free: \n      *{}\n*".format(report_disk_total, report_disk_used,report_disk_free)
+    #report_tv="-New Added Movie:\n     *Arrow S08E01*\n\n"
+    #report_spacing="~~_DISK USAGE_~~\n -Total: \n      *{}*\n -Use: \n      *{}* \n-Free: \n      *{}\n*".format(report_disk_total, report_disk_used,report_disk_free)
     report_title=report_movie+report_tv + report_spacing
     telegram_bot_sendtext(report_movie)
 
-schedule.every().day.at("22:52").do(report)
+schedule.every().day.at("22:57").do(report)
