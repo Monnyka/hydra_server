d62e38215cd8cd61f7e3934a51899912bb7fd3a4
Monnyka <31350922+Monnyka@users.noreply.github.com>
2020-08-02 23:07:38 +0700

    mass

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
 import time
 import schedule
-import shutil
-import telebot
 import os
 
-bot = telebot.TeleBot('1107438786:AAED_PIcm4FYHNX_zCLJBGDktDVpAvf7g5Q')
-
-# def telegram_bot_sendtext(bot_message):
-#     bot_token = '1107438786:AAED_PIcm4FYHNX_zCLJBGDktDVpAvf7g5Q'
-#     bot_chatID = '342923927'
-#     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-#     response = requests.get(send_text)
-#     return response.json()
-
-# def report():
-#     report_movie="*Report for Hydra Server:* \n\n ~~_MEDIA:_~~\n -New Added Movie:\n     *American Sniper (2020)*\n"
-#     #report_tv="-New Added Movie:\n     *Arrow S08E01*\n\n"
-#     #report_spacing="~~_DISK USAGE_~~\n -Total: \n      *{}*\n -Use: \n      *{}* \n-Free: \n      *{}\n*".format(report_disk_total, report_disk_used,report_disk_free)
-#     #report_title=report_movie+report_tv + report_spacing
-#     telegram_bot_sendtext(report_movie)
-
 def start_minecraft():
-    os.system("sudo systemctl start minecraftpeserver.service")
-    print("Successfully start minecraftpeserver")
+        os.system("sudo systemctl start minecraftpeserver.service")
+        print("Successfully start minecraftpeserver")
 
 def stop_minecraft():
-    os.system("sudo systemctl stop minecraftpeserver.service")
-    print("Successfully stop minecraftserver")
+        os.system("sudo systemctl stop minecraftpeserver.service")
+        print("Successfully stop minecraftserver")
 
 
 schedule.every().day.at("08:00").do(start_minecraft)
-schedule.every().day.at("22:05").do(stop_minecraft)
+schedule.every().day.at("23:00").do(stop_minecraft)
 schedule.every().day.at("13:00").do(start_minecraft)
 schedule.every().day.at("23:00").do(stop_minecraft)
 
 while True:
-    schedule.run_pending()
-    time.sleep(1)
+        schedule.run_pending()
+        time.sleep(1)
 
 
